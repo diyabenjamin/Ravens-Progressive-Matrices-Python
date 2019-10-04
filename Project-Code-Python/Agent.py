8049dfa46af869929775870b2f70b0f236f2e26b
Diya Benjamin <diyabenjamin@gatech.edu>
2019-10-04 01:14:45 -0400

    error-check-2 2nd submission

--- Project-Code-Python/Agent.py
+++ Project-Code-Python/Agent.py
@@ -36,18 +36,32 @@ class Agent:
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
 
-    image_numbers = [None] * 7
+    image_numbers = [None] * 9
     image_a = None
     image_b = None
     image_c = None
+    image_d = None
+    image_e = None
+    image_f = None
+    image_g = None
+    image_h = None
 
     def setup_global_variables(self, problem):
-        for i in range(1, 7):
-            self.image_numbers[i] = Image.open(problem.figures[str(i)].visualFilename)
-
         self.image_a = Image.open(problem.figures['A'].visualFilename)
         self.image_b = Image.open(problem.figures['B'].visualFilename)
         self.image_c = Image.open(problem.figures['C'].visualFilename)
+        image_numbers_count = 7
+
+        if problem.problemType == '3x3':
+            self.image_d = Image.open(problem.figures['D'].visualFilename)
+            self.image_e = Image.open(problem.figures['E'].visualFilename)
+            self.image_f = Image.open(problem.figures['F'].visualFilename)
+            self.image_g = Image.open(problem.figures['G'].visualFilename)
+            self.image_h = Image.open(problem.figures['H'].visualFilename)
+            image_numbers_count = 9
+
+        for i in range(1, image_numbers_count):
+            self.image_numbers[i] = Image.open(problem.figures[str(i)].visualFilename)
 
     def Solve(self, problem):
         print problem.name
@@ -55,15 +69,95 @@ class Agent:
 
         self.setup_global_variables(problem)
 
-        option = self.solve_image_difference() if option == -1 else option
-        option = self.solve_image_reflection() if option == -1 else option
-        option = self.solve_image_rotation() if option == -1 else option
-        option = self.solve_image_fill() if option == -1 else option
-        option = self.solve_image_vertices_count() if option == -1 else option
-
-        print option
-
+        if problem.problemType == '2x2':
+            option = self.solve_image_difference() if option == -1 else option
+            option = self.solve_image_reflection() if option == -1 else option
+            option = self.solve_image_rotation() if option == -1 else option
+            option = self.solve_image_fill() if option == -1 else option
+            option = self.solve_image_vertices_count() if option == -1 else option
+        elif problem.problemType == '3x3':
+            option = self.solve_image_difference_3x3() if option == -1 else option
         return option
+
+    # 3x3 functions starts from here
+
+    def solve_image_difference_3x3(self):
+        # check for difference between image a and b, then find answer that has similar transition with c
+
+        # ImageChops.invert(ImageChops.subtract(self.image_a, self.image_b)).show()
+        # ImageChops.invert(ImageChops.subtract(self.image_b, self.image_c)).show()
+
+        # print 'a-b:', self.get_similarity_ratio(self.image_a, self.image_b)
+        # print 'b-c:', self.get_similarity_ratio(self.image_b, self.image_c)
+        # print 'd-e:', self.get_similarity_ratio(self.image_d, self.image_e)
+        # print 'e-f:', self.get_similarity_ratio(self.image_e, self.image_f)
+        # print 'g-h:', self.get_similarity_ratio(self.image_g, self.image_h)
+        # print 'h-1:', self.get_similarity_ratio(self.image_h, self.image_numbers[1])
+        # print 'h-2:', self.get_similarity_ratio(self.image_h, self.image_numbers[2])
+        # print 'h-3:', self.get_similarity_ratio(self.image_h, self.image_numbers[3])
+        # print 'h-4:', self.get_similarity_ratio(self.image_h, self.image_numbers[4])
+        # print 'h-5:', self.get_similarity_ratio(self.image_h, self.image_numbers[5])
+        # print 'h-6:', self.get_similarity_ratio(self.image_h, self.image_numbers[6])
+        # print 'h-7:', self.get_similarity_ratio(self.image_h, self.image_numbers[7])
+        # print 'h-8:', self.get_similarity_ratio(self.image_h, self.image_numbers[8])
+
+        diff_image_a_b = self.get_similarity_ratio(self.image_a, self.image_b)
+        diff_image_b_c = self.get_similarity_ratio(self.image_b, self.image_c)
+        diff_image_d_e = self.get_similarity_ratio(self.image_d, self.image_e)
+        diff_image_e_f = self.get_similarity_ratio(self.image_e, self.image_f)
+        diff_image_g_h = self.get_similarity_ratio(self.image_g, self.image_h)
+
+        # if (round(diff_image_a_b, 2) == round(diff_image_b_c, 2)) and (
+        #         round(diff_image_d_e, 2) == round(diff_image_e_f, 2)):
+        #     for i in range(1, 9):
+        #         diff_image_h_i = self.get_similarity_ratio(self.image_h, self.image_numbers[i])
+        #         if round(diff_image_g_h, 2) == round(diff_image_h_i, 2):
+        #             return i
+
+        diff_row_1 = diff_image_a_b - diff_image_b_c
+        diff_row_2 = diff_image_d_e - diff_image_e_f
+
+        # print 'diff row1-row2: ', round(diff_row_1 / diff_row_2, 2)
+        if round(diff_row_1 / diff_row_2, 2) == 1:
+            print 'inside diff of 1'
+            option = 0
+            min_difference = 1
+            for i in range(1, 9):
+                diff_image_h_i = self.get_similarity_ratio(self.image_h, self.image_numbers[i])
+                diff_row_3 = diff_image_g_h - diff_image_h_i
+                difference = abs(round(diff_row_2 / diff_row_3, 2))
+                # print i, diff_row_3, difference, 1 - difference
+                if abs(1 - difference) < min_difference:
+                    option = i
+                    min_difference = abs(1 - difference)
+            if option > 0 and min_difference < 0.1:
+                print "1*****", min_difference
+                return option
+
+        diff_row_1_2 = diff_row_1 - diff_row_2
+        expected_diff_row_3 = diff_row_2 - diff_row_1_2
+
+        # print "diff_row_1:", diff_row_1, " diff_row_2:", diff_row_2
+        # print "diff_row_1_2:", diff_row_1_2, "expected_diff_row_3:", expected_diff_row_3
+
+        print 'inside diff of 2'
+        option = 0
+        min_difference = 1
+        for i in range(1, 9):
+            diff_image_h_i = self.get_similarity_ratio(self.image_h, self.image_numbers[i])
+            diff_row_3 = diff_image_g_h - diff_image_h_i
+            difference = expected_diff_row_3 - diff_row_3
+            # print i, expected_diff_row_3, diff_row_3, difference
+            if 0 < difference < min_difference:
+                option = i
+                min_difference = difference
+        if option > 0 and min_difference < 0.01:
+            print "2*****", min_difference
+            return option
+
+        return -1
+
+    # 2x2 functions starts from here
 
     def solve_image_difference(self):
         # check for difference between image a and b, then find answer that has similar transition with c
@@ -77,7 +171,6 @@ class Agent:
                 option = i
                 max_similarity_ratio = similarity_ratio
         if option > 0:
-            # print 'Basic B-10/B-11'
             return option
 
         # check for difference between image a and c, then find answer that has similar transition with b
@@ -91,7 +184,6 @@ class Agent:
                 option = i
                 max_similarity_ratio = similarity_ratio
         if option > 0:
-            # print '**********'
             return option
 
         return -1
@@ -103,7 +195,6 @@ class Agent:
         index = 0
         curr_max_val = 0
         if self.get_similarity_ratio(image_at, self.image_b) > HIGH_SIMILARITY_THRESHOLD:
-            # print 'flip left-right A and B are same'
             for i in range(1, 7):
                 similarity_ratio_ct_numbers = self.get_similarity_ratio(image_ct, self.image_numbers[i])
                 if similarity_ratio_ct_numbers > LOW_SIMILARITY_THRESHOLD and similarity_ratio_ct_numbers > curr_max_val:
@@ -118,7 +209,6 @@ class Agent:
         index = 0
         curr_max_value = 0
         if self.get_similarity_ratio(image_at, self.image_c) > HIGH_SIMILARITY_THRESHOLD:
-            # print 'flip top-down A and C are same'
             for i in range(1, 7):
                 similarity_ratio_bt_numbers = self.get_similarity_ratio(image_bt, self.image_numbers[i])
                 if similarity_ratio_bt_numbers > LOW_SIMILARITY_THRESHOLD and similarity_ratio_bt_numbers > curr_max_value:
@@ -142,9 +232,7 @@ class Agent:
                 lambda i: i < 128 and 255))
             index = 0
             max_ratio = 0
-            # print round(similarity_ratio_ar_b, 2)
             if round(similarity_ratio_ar_c, 2) >= LOW_SIMILARITY_THRESHOLD:
-                # print 'A rotate and C are same'
                 for i in range(1, 7):
                     image_number = self.image_numbers[i].convert('L').point(lambda i: i < 128 and 255)
                     similarity_ratio_br_numbers = self.get_similarity_ratio(image_br, image_number)
@@ -155,7 +243,6 @@ class Agent:
                 if index > 0:
                     return index
             elif round(similarity_ratio_ar_b, 2) >= LOW_SIMILARITY_THRESHOLD:
-                # print 'A rotate and B are same'
                 for i in range(1, 7):
                     image_number = self.image_numbers[i].convert('L').point(lambda i: i < 128 and 255)
                     similarity_ratio_cr_numbers = self.get_similarity_ratio(image_cr, image_number)
@@ -181,7 +268,6 @@ class Agent:
                     option = i
                     max_similarity_ratio = similarity_ratio
             if option > 0:
-                # print 'Fill image - Basic B-09'
                 return option
         return -1
 
@@ -195,14 +281,12 @@ class Agent:
             dif_vertices_c_ab = c_vertices - diff_vertices_a_b
             for i in range(1, 7):
                 if dif_vertices_c_ab == self.get_number_of_vertices(self.image_numbers[i]):
-                    # print 'Vertices count'
                     return i
         elif b_vertices > a_vertices:
             diff_vertices_a_b = b_vertices - a_vertices
             dif_vertices_c_ab = c_vertices + diff_vertices_a_b
             for i in range(1, 7):
                 if dif_vertices_c_ab == self.get_number_of_vertices(self.image_numbers[i]):
-                    # print 'Vertices count'
                     return i
 
         return -1
